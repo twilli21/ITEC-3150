2cada678ddf95ae5525845ad5ce946241446420a
Taylor Williams <50511111+twilli21@users.noreply.github.com>
2020-06-12 03:57:45 -0400

    Add files via upload

---
+++ ITEC3150_JavaFX2/src/ch15b/animationEtc_new/TimelineDemo_2a.java
@@ -0,0 +1,81 @@
+package ch15b.animationEtc_new;
+
+import javafx.animation.Animation;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.util.Duration;
+
+public class TimelineDemo_2a extends Application {
+  @Override // Override the start method in the Application class
+  public void start(Stage primaryStage) {
+    StackPane pane = new StackPane();
+    Text text = new Text(20, 50, "Programming is fun");
+    text.setFill(Color.DARKORANGE);
+    pane.getChildren().add(text); // Place text into the stack pane
+
+    // Create a handler for changing text
+    EventHandler<ActionEvent> eventHandler = 
+    		e -> {
+    			if (text.getText().length() != 0) {
+    				text.setText("");
+    			}
+    			else {
+    				text.setText("Programming is fun");
+    			}
+    		};
+    
+    /*The following is equivalent to line 26-33
+    new EventHandler<ActionEvent> () {
+		@Override
+		public void handle(ActionEvent e) {
+			if (text.getText().length() != 0) {
+                text.setText("");
+            }
+            else {
+                text.setText("Programming is fun");
+            }
+		}
+	}*/
+    
+    // Create an animation for alternating text
+    Timeline animation = new Timeline(
+      new KeyFrame(Duration.millis(500), eventHandler));
+    animation.setCycleCount(Timeline.INDEFINITE);
+    
+    animation.play(); // Start animation
+    
+    // You can also add code to pause and resume animation
+    /*
+    text.setOnMouseClicked(e -> {
+      if (animation.getStatus() == Animation.Status.PAUSED) {
+        animation.play();
+      }
+      else {
+        animation.pause();
+      }
+    });
+    */
+    
+    // Create a scene and place it in the stage
+    Scene scene = new Scene(pane, 250, 50);
+    primaryStage.setTitle("TimelineDemo"); // Set the stage title
+    primaryStage.setScene(scene); // Place the scene in the stage
+    primaryStage.show(); // Display the stage
+  }
+
+  /**
+   * The main method is only needed for the IDE with limited
+   * JavaFX support. Not needed for running from the command line.
+   */
+  public static void main(String[] args) {
+    launch(args);
+  }
+}
